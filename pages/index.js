17a84360ef64a25890e27a0359b9f15a24b1ab08
aakrut <aakrutdabhi@gmail.com>
2022-10-03 19:46:01 +0530

    add new components

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import data from "../constants/mock-nft.json";
+import mockartist from "../constants/mock-artist.json";
 
 export default function Home() {
   return (
@@ -12,15 +13,15 @@ export default function Home() {
       </Head>
 
       {/* HeroSection */}
-      <div className="bg-[#1242ef] absolute left-[-250px] top-[-210px] h-[352px] w-[652px] blur-[350px] rounded-full"></div>
+      <div className="bg-[#1242ef] absolute left-[-250px] top-[-210px] h-[352px] w-[652px] blur-[350px] rounded-full "></div>
 
       {/* HeroSection */}
-      <section className="max-w-[1240px] my-20 mx-auto grid grid-cols-2  gap-2 font-body h-[540px] overflow-hidden top-7">
-        <div className="flex flex-col items-start justify-center h-full">
-          <h1 className="w-full ">
+      <section className="max-w-[1240px] my-20 mx-auto grid grid-cols-2  gap-2 font-body h-[540px] overflow-hidden top-7 md:gap-12 medium md:px-5 sm:grid-cols-1 sm:h-full">
+        <div className="flex flex-col items-start justify-center h-full sm:items-center">
+          <h1 className="w-full text-4xl sm:text-center">
             Discover New Era of <br /> Crypto Currencies
           </h1>
-          <p className="text-[#ADB9C7]">
+          <p className="text-[#ADB9C7] sm:text-center">
             Kittos is the primier marketplace for NFT, which are digital items
             you can truly own. Digital items have existed for a long time, but
             never like this.
@@ -29,11 +30,11 @@ export default function Home() {
             type="button"
             className="bg-[#1E50FF] outline-none border-none py-3 px-5 rounded-xl font-body cursor-pointer transition duration-250 ease-in-out hover:scale-125 hover:drop-shadow-xl hover:shadow-sky-600 w-auto"
           >
-            Get Started
+            Connect Wallet
           </button>
         </div>
         <div className="w-full flex items-center justify-center">
-          <div className="w-[400px] h-[536px] bg-[#272D37]/60 rounded-2xl flex flex-col p-6">
+          <div className="w-[400px] h-[536px] bg-[#272D37]/60 rounded-2xl flex flex-col p-6 sm:h-max">
             <Image
               src="/images/mock.png"
               alt="mock"
@@ -67,10 +68,10 @@ export default function Home() {
 
       <div className="bg-[#1242ef] absolute left-[1033px] top-[655px] h-[352px] w-[652px] blur-[350px] rounded-full"></div>
 
-      <section className="max-w-[1240px] my-20 mx-auto  gap-2 font-body overflow-hidden top-7 ">
+      <section className="max-w-[1240px] my-20 mx-auto  gap-2 font-body top-7 ">
         <h1 className="text-center w-full">Create and sell your NFTs</h1>
 
-        <div className="grid grid-cols-3 gap-5 sm:grid-cols-1 sm:p-12">
+        <div className="grid grid-cols-3 gap-5 sm:grid-cols-1 sm:p-12 md:grid-cols-1 md:mx-10">
           {data.map((item) => (
             <div
               key={item.id}
@@ -83,12 +84,60 @@ export default function Home() {
                   className="w-full h-full"
                 />
               </div>
-              <h4 className="font-bold text-[24px] md:text-[14px]">{item.title}</h4>
+              <h4 className="font-bold text-[24px] md:text-[14px]">
+                {item.title}
+              </h4>
               <p className="text-center text-[#ADB9C7] text-[14px]">
                 {item.description}
               </p>
             </div>
           ))}
+        </div>
+      </section>
+
+      <section className="max-w-[1240px] my-20 mx-auto  gap-2 font-body top-7 ">
+        <h2 className="sm:text-center md:mx-10">Featured Artist </h2>
+        <div className="grid grid-cols-4 gap-3 sm:gap-y-8 md:grid-cols-2 sm:grid-cols-1 sm:p-12 md:mx-10">
+          {mockartist.map((data) => (
+            <div
+              key={data.id}
+              className="w-full bg-[#272D37] flex flex-col justify-center items-center p-3 rounded-xl"
+            >
+              <div className="w-full relative">
+                <img
+                  src={data.bgImage}
+                  alt={data.name}
+                  layout="responsive"
+                  className="w-full rounded-2xl h-[225px]"
+                />
+                <img
+                  src={data.image}
+                  alt={data.name}
+                  layout="intrinsic"
+                  className="absolute -bottom-[40px] left-0 right-0 mx-auto h-[104px] w-[104px] bg-[#272D37] rounded-full"
+                />
+              </div>
+              <div className="w-full text-center mt-8 font-bold">
+                <h3>{data.name}</h3>
+                <p>Price: {data.price}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* Community */}
+      <section className="max-w-[1240px] bg- my-20 mx-auto bg-[#272D37] gap-2 font-body top-7 text-center p-10 rounded-xl sm:mx-10 md:m-10">
+        <div>
+          <h1 className="text-4xl sm:text-2xl">Create Your Own NFT!</h1>
+          <p className="text-[#ADB9C7] px-[120px] sm:p-2 ms:p-1">
+            We have a large scale group to support each other in this game, Join
+            us to get the news as soon as possible and follow our latest
+            announcements!
+          </p>
+          <button className="bg-[#1E50FF] outline-none border-none py-3 px-5 rounded-xl font-body cursor-pointer transition duration-250 ease-in-out hover:scale-125 hover:drop-shadow-xl hover:shadow-sky-600 w-auto">
+            Join Comunity Now
+          </button>
         </div>
       </section>
     </div>
